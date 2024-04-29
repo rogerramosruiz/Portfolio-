46eb054bb5f6a6e16aba663ca3ff21ee8386ee07
rogerramosruiz <rogerramosruizdev@gmail.com>
2024-04-29 18:05:36 -0400

    fix Projects

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Card from '../components/Card'
 import { projects } from '../lib/projects'
+import { motion } from 'framer-motion'
+
 export default function Projects() {
   return (
     <div>
       {projects.map((project, i) => (
-        <Card {...project} />
+        <motion.div animate='show' className='overflow-hidden mx-5 xl:m-28'>
+          <Card {...project} index={i} />
+        </motion.div>
       ))}
     </div>
   )
