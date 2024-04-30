a0e8ab89eb043f2660cb9d83e691dff654d249bb
rogerramosruiz <rogerramosruizdev@gmail.com>
2024-04-29 20:02:35 -0400

    footer links new tab

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,13 @@ export default function Footer() {
   return (
     <footer className='bg-gray-800 text-white py-4'>
       <div className='flex justify-center gap-4'>
-        <a href='https://github.com/'>
+        <a href='https://github.com/' target='_blank' rel='noreferrer'>
           <FaGithub size={30} />
         </a>
-        <a href='https://www.linkedin.com/'>
+        <a href='https://www.linkedin.com/' target='_blank' rel='noreferrer'>
           <FaLinkedin size={30} />
         </a>
-        <a href='https://hub.docker.com/'>
+        <a href='https://hub.docker.com/' target='_blank' rel='noreferrer'>
           <FaDocker size={30} />
         </a>
       </div>
