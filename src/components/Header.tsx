c3c18f7d9050570bff768bfbf8466941a289582b
rogerramosruiz <rogerramosruizdev@gmail.com>
2024-04-29 20:05:43 -0400

    fix header overlap

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,8 +18,7 @@ export default function Header() {
   })
 
   return (
-    <div className='bg-blue-800 p-5 h-screen flex items-center'>
-      
+    <div className='bg-blue-800 p-5 min-h-screen flex items-center'>
       <div className='w-full flex justify-center gap-2 flex-col md:flex-row'>
         <CanvasMatrix />
         <div className='w-full lg:w-2/5 xl:w-1/5 flex items-center justify-center'>
