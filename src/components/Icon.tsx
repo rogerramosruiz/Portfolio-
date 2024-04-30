f02fd2c29d7fe34f3bdc47f5ba803c49185d2a38
rogerramosruiz <rogerramosruizdev@gmail.com>
2024-04-30 14:50:29 -0400

    hover icons size

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -4,7 +4,7 @@ export default function Icon({ icon }: { icon: React.ReactNode }) {
   return (
     <div>
       {React.cloneElement(icon as React.ReactElement<any>, {
-        className: 'size-8 hover:size-7 transition-all duration-100 ',
+        className: 'size-8 hover:size-9 transition-all duration-100 ',
       })}
     </div>
   )
