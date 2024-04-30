92b21646fa6331841907c468ed049088fedefa60
rogerramosruiz <rogerramosruizdev@gmail.com>
2024-04-30 07:33:46 -0400

    project title

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -6,6 +6,7 @@ import { motion } from 'framer-motion'
 export default function Projects() {
   return (
     <div>
+      <h1 className='text-5xl font-bold text-center pb-4'>Projects</h1>
       {projects.map((project, i) => (
         <motion.div animate='show' className='overflow-hidden mx-5 xl:m-28'>
           <Card {...project} index={i} />
